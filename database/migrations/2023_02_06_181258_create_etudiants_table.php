a773cf6ecd7bf5131f893f99792330111151b7d0
Edgar <mmnledgar@gmail.com>
2023-02-06 18:26:34 +0000

    adding project

---
+++ database/migrations/2023_02_06_181258_create_etudiants_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("surname");
+            $table->string("phone_number")->unique();
+            $table->float("note_one");
+            $table->string("exam_note");
+
+            // relationships with matieres table
+            $table->unsignedBigInteger('matiere_id');
+            $table->foreign('matiere_id')->references('id')->on('matieres')
+            ->cascadeOnUpdate()->nullOnDelete();
+
+            // relationship with semestres table
+            $table->unsignedBigInteger('semestre_id');
+            $table->foreign('semestre_id')->references('id')->on('semestres')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+}
