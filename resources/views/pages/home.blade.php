558ca20ca1ea0d56b26c97222189ac354e6a058c
Edgar <mmnledgar@gmail.com>
2023-02-06 18:54:41 +0000

    adding the page

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layout.admin_layout')
+
+
+@section("main")
+
+<section class="panel important">
+    <h2>Write Some News</h2>
+    <ul>
+      <li>Information Panel</li>
+    </ul>
+  </section>
+  
+  <section class="panel important">
+    <h2>Write a post</h2>
+      <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+        <div class="twothirds">
+          Blog title:<br/>
+          <input type="text" name="title" size="40"/><br/><br/>
+          Content:<br/>    
+          <textarea name="newstext" rows="15" cols="67"></textarea><br/>  
+        </div>
+        <div>
+          <input type="submit" name="submit" value="Save" />
+        </div>
+        </div>
+      </form>
+  </section>
+
+
+@endsection
